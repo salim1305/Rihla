fb262bb5d900fe66342652318ebbcc3020aa4d07
salim1305 <salim.dafaa@gmail.com>
2025-08-23 00:33:51 +0100

    fix: import Joi en haut et dépendances pour Railway

--- dist/validators/userValidator.js
+++ dist/validators/userValidator.js
@@ -4,11 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.updateUserSchema = exports.registerSchema = exports.loginSchema = void 0;
+const joi_1 = __importDefault(require("joi"));
 exports.loginSchema = joi_1.default.object({
     email: joi_1.default.string().email().required(),
     password: joi_1.default.string().min(6).required(),
 });
-const joi_1 = __importDefault(require("joi"));
 exports.registerSchema = joi_1.default.object({
     email: joi_1.default.string().email().required(),
     password: joi_1.default.string().min(6).required(),
--- src/validators/userValidator.ts
+++ src/validators/userValidator.ts
@@ -1,8 +1,10 @@
+
+import Joi from "joi";
+
 export const loginSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
 });
-import Joi from "joi";
 
 export const registerSchema = Joi.object({
   email: Joi.string().email().required(),
